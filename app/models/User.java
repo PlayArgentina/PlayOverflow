618fbb91fc3a0e425843bd6c7073dd6e36a34dfd
Cristian Pereyra <criis.pereyra@gmail.com>
2012-05-05 11:31:23 -0300

    rutas de users

--- app/models/User.java
+++ app/models/User.java
@@ -24,7 +24,7 @@ public class User extends Model {
     @Constraints.Required
     public String password;
     
-    @ManyToOne
+    @OneToMany
     public List<Question> questions;
     
 //    /**
