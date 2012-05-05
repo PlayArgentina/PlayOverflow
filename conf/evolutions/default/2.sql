538e09911448d37b04f44aec1bbe3b531ebfeef1
Marcos Nils <marcosnils@gmail.com>
2012-05-05 13:52:55 -0300

    Se modifica el 2.sql para las respuestas

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,6 +2,7 @@
 
 # --- !Ups
 insert into user (id,username,password) values (  1,'derfarg','123456');
+insert into user (id,username,password) values (  2,'marcosnils','123456');
 
 insert into question (id,title,description,tags,user_id, publish_date) values (  1,'Apple Inc.','App fixture description','fixure',1, '1991-01-01');
 insert into question (id,title,description,tags,user_id, publish_date) values (  2,'Thinking Machines','App fixture description','fixure',1, '2011-01-01');
@@ -11,6 +12,10 @@ insert into question (id,title,description,tags,user_id, publish_date) values ( 
 insert into question (id,title,description,tags,user_id, publish_date) values (  6,'Commodore International','App fixture description','fixure',1, '1991-01-01');
 insert into question (id,title,description,tags,user_id, publish_date) values (  7,'MOS Technology','App fixture description','fixure',1, '1987-01-01');
 insert into question (id,title,description,tags,user_id, publish_date) values (  8,'Micro Instrumentation and Telemetry Systems','App fixture description','fixure',1,'1991-01-20');
+
+insert into answer (id,description,user_id) values (  1,'App fixture answer',2);
+
+
 # --- !Downs
 
 
