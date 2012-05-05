05f2a9f1736eef04027d001afc3741df81c9df7a
Cristian Pereyra <criis.pereyra@gmail.com>
2012-05-05 12:09:00 -0300

    updates en la config y login de signup

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,15 +1,44 @@
 package controllers;
 
-import play.*;
+import java.util.*;
+
 import play.mvc.*;
+import play.data.*;
+import play.*;
 
 import views.html.*;
 
+import models.*;
+
 public class Users extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Users."));
-  }
-  
-  
+  private static Form<User> localForm = form(User.class);
+
+	public static Result signUp() {
+		Form<User> userForm = localForm;
+		return ok(views.html.users.signup.render(userForm));
+	}
+
+	public static Result doSignUp() {
+    Form<User> userForm = localForm.bindFromRequest();
+        if(userForm.hasErrors()) {
+            return badRequest(views.html.users.signup.render(userForm));
+        }
+        userForm.get().save();
+        flash("success", "Computer " + userForm.get().username + " has been created");
+        return Application.GO_HOME;
+	}
+
+	public static Result login() {
+		return null;
+	}
+
+	public static Result doLogin() {
+		return null;
+	}
+
+	public static Result doLogout() {
+		// return ok(index.render("Users."));
+		return null;
+	}
+
 }
