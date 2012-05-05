d8488cd7fea618f0cc655287c34670562faaaf59
Marcos Nils <marcosnils@gmail.com>
2012-05-05 15:05:54 -0300

    Se inicia la vista para las preguntas

--- app/controllers/Questions.java
+++ app/controllers/Questions.java
@@ -10,7 +10,19 @@ public class Questions extends Controller {
   
   public static Result index() {
     return ok(views.html.questions.list.render(Question.find.all()));
+  
   }
   
   
+  public static Result showQuestion(String questionId) {
+	  	Question question = Question.find.byId(Long.valueOf(questionId)); 
+	  		
+	  	if (question == null) {
+	  		return notFound("Question not found");
+  	} else {
+  		return ok(views.html.questions.view.render(question);
+  	}
+  	
+  }
+
 }
