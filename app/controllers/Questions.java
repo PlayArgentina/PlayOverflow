f37e336f2b281764e9059be55da389e0f36dbdb4
Marcos Nils <marcosnils@gmail.com>
2012-05-05 15:14:57 -0300

    Se arregla el error en Question Controller y el route

--- app/controllers/Questions.java
+++ app/controllers/Questions.java
@@ -7,22 +7,19 @@ import play.mvc.*;
 import views.html.*;
 
 public class Questions extends Controller {
-  
-  public static Result index() {
-    return ok(views.html.questions.list.render(Question.find.all()));
-  
-  }
-  
-  
-  public static Result showQuestion(String questionId) {
+
+	public static Result index() {
+		return ok(views.html.questions.list.render(Question.find.all()));
+
+	}
+
+	public static Result showQuestion(String questionId) {
 	  	Question question = Question.find.byId(Long.valueOf(questionId)); 
 	  		
 	  	if (question == null) {
 	  		return notFound("Question not found");
   	} else {
-  		return ok(views.html.questions.view.render(question);
+  		return ok(views.html.questions.view.render(question));
   	}
-  	
   }
-
 }
